abcb53351b795860bfef54c5248ab96037ad42de
Andrii Slynko <qkston22@gmail.com>
2025-02-24 23:48:16 +0200

    Remove unused CSS import from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Amplify } from "aws-amplify";
-import "./index.css";
 import App from "./App";
 import awsconfig from "./aws-exports";
 
