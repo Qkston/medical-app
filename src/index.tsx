048a9f8325fe89e6f5f817c84244eaffc579b4e7
Andrii Slynko <qkston22@gmail.com>
2025-02-24 23:43:10 +0200

    Move aws-exports.js to src directory and update import path

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Amplify } from "aws-amplify";
 import "./index.css";
 import App from "./App";
-import awsconfig from "../amplify/aws-exports";
+import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 
